145bb62019b6705b4423ca42e808f36cb2edb2ee
Christoph Nitsche <Free.88@outlook.de>
2024-04-19 00:16:17 +0200

    SupportMessage Fertig

--- DelikatessenDrehbuch/Controllers/SupportController.cs
+++ DelikatessenDrehbuch/Controllers/SupportController.cs
@@ -6,14 +6,15 @@ using DelikatessenDrehbuch.Data;
 
 namespace DelikatessenDrehbuch.Controllers
 {
-    
+
     public class SupportController : Controller
     {
-       private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public SupportController(ApplicationDbContext context)
         {
-            _context = context;  
+
+            _context = context;
         }
 
 
@@ -23,7 +24,7 @@ namespace DelikatessenDrehbuch.Controllers
         }
 
         [HttpPost]
-        public IActionResult CreatSupportMessage([FromBody]SupportMessage message)
+        public IActionResult CreatSupportMessage([FromBody] SupportMessage message)
         {
             if (message == null)
                 return BadRequest();
@@ -31,7 +32,23 @@ namespace DelikatessenDrehbuch.Controllers
             _context.SupportMessage.Add(message);
             _context.SaveChanges();
 
-           return Ok();
+            return Ok();
+        }
+
+        public IActionResult DeleteSupportTicket(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            var supportMessageFromDb = _context.SupportMessage.SingleOrDefault(x => x.Id == id);
+
+            if (supportMessageFromDb == null)
+                return BadRequest();
+
+            _context.Remove(supportMessageFromDb);
+            _context.SaveChanges();
+
+            var newSupportMessageFromDb = _context.SupportMessage.ToList();
+            return PartialView("~/Views/MyRecipes/_AdminSupportMessage.cshtml", newSupportMessageFromDb);
         }
     }
 }
