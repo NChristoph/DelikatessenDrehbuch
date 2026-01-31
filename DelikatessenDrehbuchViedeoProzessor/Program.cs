d08999f57c3da3794e2881207354816c811d4c54
NChristoph <105702601+NChristoph@users.noreply.github.com>
2026-01-31 23:20:25 +0100

    Make ffmpeg path OS-aware for Linux app service

--- DelikatessenDrehbuchViedeoProzessor/Program.cs
+++ DelikatessenDrehbuchViedeoProzessor/Program.cs
@@ -1,9 +1,21 @@
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+var blobStorageConnection = builder.Configuration.GetValue<string>("BlobStorageConnection")
+    ?? builder.Configuration.GetValue<string>("AzureWebJobsStorage");
+
+if (string.IsNullOrWhiteSpace(blobStorageConnection))
+{
+    throw new InvalidOperationException("Blob storage connection string is missing. Configure 'BlobStorageConnection' or 'AzureWebJobsStorage'.");
+}
+
+builder.Services.AddSingleton(new BlobServiceClient(blobStorageConnection));
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
