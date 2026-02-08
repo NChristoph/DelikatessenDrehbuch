0153715a4c366053135cc02f1cf4258d40b8b91a
NChristoph <105702601+NChristoph@users.noreply.github.com>
2026-02-08 22:33:10 +0100

    newMealPlanerLayout

--- DelikatessenDrehbuch/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DelikatessenDrehbuch/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,16 +1,12 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using Microsoft.AspNetCore.Authentication;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace DelikatessenDrehbuch.Areas.Identity.Pages.Account
 {
