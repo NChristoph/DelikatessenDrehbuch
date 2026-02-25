7f587a8f6964d077e738eac70e783e662101c346
NChristoph <105702601+NChristoph@users.noreply.github.com>
2026-02-25 10:56:59 +0100

    NewMasterTamplate

--- DelikatessenDrehbuch/Areas/WorldMiniApp/Controllers/HomeController.cs
+++ DelikatessenDrehbuch/Areas/WorldMiniApp/Controllers/HomeController.cs
@@ -20,7 +20,7 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace DelikatessenDrehbuch.Areas.WorldMiniApp.Controllers
 {
-    //TODO:in claud weiter machen
+   
     [Area("WorldMiniApp")]
     public class HomeController : Controller
     {
